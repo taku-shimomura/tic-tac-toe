7a94c87bdcac9c1f1370c346deec026873bfbcb4
Taku <tgmnk81996@gmail.com>
2019-10-30 15:14:28 +0900

    Create Store with src/index.tsx and add Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import './index.css';
-import Game from './components/Game';
+import reducer from './reducers/reducer'
+import GameContainer from './containers/GameContainer'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+const store = createStore(reducer)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <GameContainer />
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
