508131125ed934b186b3bdbc30bc4d37a386e32c
takkn <tgmnk81996@gmail.com>
2019-11-10 16:31:28 +0900

    Ended implement of handleClick

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import { gameState } from '../reducers/reducer'
-import { GameActions } from '../containers/GameContainer'
-import Board from './Board'
-import '../style.css'
+import React from "react";
+import { gameState } from "../reducers/reducer";
+import { GameActions } from "../containers/GameContainer";
+import Board from "./Board";
+import "../style.css";
 
-type IProps = gameState & GameActions
+type IProps = gameState & GameActions;
 
 const Game = (props: IProps) => {
-    console.log(props)
-    console.log(props.history)
-    const current = props.history[props.stepNumber]
-    return(
-        <div>
-            <div>
-                <Board squares={current.squares} onClick={props.handleClick} />
-            </div>
-            <div>
-            </div>
-        </div>
-    )
-}
+  console.log("GameComponent Debug Start");
+  console.log("stepNumber");
+  console.log(props.stepNumber);
+  console.log("history");
+  console.log(props.history[0].squares);
+  console.log("GameComponent Debug End");
+  // const current = props.history[props.stepNumber]
+  const current = props.history[0].squares;
+  console.log("current");
+  console.log(current);
+  return (
+    <div>
+      <div>
+        {/* <Board squares={current.squares} onClick={props.handleClick} /> */}
+        <Board squares={current} onClick={props.handleClick} />
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default  Game
+export default Game;
--- src/reducers/reducer.ts
+++ src/reducers/reducer.ts
@@ -25,6 +25,15 @@ const initialState: gameState = {
 export const gameReducer = reducerWithInitialState(initialState)
     .case(gameActions.handleClick, (state, i) => {
         //クリックしたら x か ◯ がつく。 
+        const history = state.history.slice(0, state.stepNumber + 1)
+        const current = history[history.length - 1]
+        const squares = current.squares.slice()
+        if(!squares[i]) {
+            squares[i] = state.xIsNext ? "X" : "○"
+            current.squares = squares
+            state.stepNumber = history.length
+            state.xIsNext = !state.xIsNext
+        }
         return Object.assign({}, state) 
     })
 
